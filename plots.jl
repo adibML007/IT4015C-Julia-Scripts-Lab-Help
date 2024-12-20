4c3fcb5858daecb6a4a2f6f69309ffd8065d7ac9
adibML007 <a_u_z_ipe@yahoo.com>
2024-12-20 18:56:48 -0500

    Adding some plots

--- plots.jl
+++ plots.jl
@@ -3,26 +3,13 @@
 # of the Random Forest model with varying maximum depths.
 using Plots
 
-# Generate some complex data
-x = 1:100
-y1 = sin.(x .* 0.1) .* exp.(-x .* 0.01)
-y2 = cos.(x .* 0.1) .* exp.(-x .* 0.01)
-y3 = tanh.(x .* 0.1)
+x = range(0, 10, length=100)
+y1 = sin.(x)
+y2 = cos.(x)
+p = plot(x, [y1 y2])
 
-# Create a plot with multiple subplots
-plot(
-    plot(x, y1, title="Damped Sine Wave", xlabel="x", ylabel="y1", legend=false),
-    plot(x, y2, title="Damped Cosine Wave", xlabel="x", ylabel="y2", legend=false),
-    plot(x, y3, title="Tanh Function", xlabel="x", ylabel="y3", legend=false),
-    layout = (3, 1)
-)
+y3 = @. sin(x)^2 - 1/2
+plot!(p, x, y3)
 
-# Save the complex plot
-savefig("complex_plots.svg")
-# .eps, .pdf, & .png are also supported
-# we used svg here because it respects the width and height specified above
-# To display the plot in the notebook, use the following command:
-# display("complex_plots.svg", "image/svg+xml")
-# This will render the plot in the notebook as an SVG image.
-# You can also use the Plots.jl backend to display the plot directly in the notebook.
-# For more information on Plots.jl, see: https://docs.juliaplots.org/latest/
+savefig("myplot.png")      # saves the CURRENT_PLOT as a .png
+savefig(p, "myplot.pdf")   # saves the plot from p as a .pdf vector graphic
