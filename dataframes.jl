d8f19248c4c40f1ac3ad54f19c8dacba529047e6
adibML007 <a_u_z_ipe@yahoo.com>
2024-12-20 04:53:33 -0500

    Modified dataframes.jl

--- dataframes.jl
+++ dataframes.jl
@@ -2,3 +2,11 @@ x = 10;
 # Today we will learn about dataframes in Julia
 # Co-pilot is working super fine
 # I am happy with the results
+using DataFrames
+df = DataFrame(A = 1:4, B = ["M", "F", "F", "M"])
+df
+groupped = groupby(df, :B)
+for i in groupped
+    println(i)
+end
+# I am happy with the results
