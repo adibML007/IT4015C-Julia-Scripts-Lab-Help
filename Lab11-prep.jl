b907499b1defbcdf46355cd30b2fb3b0f5b65ad8
adibML007 <a_u_z_ipe@yahoo.com>
2024-12-21 04:21:13 -0500

    Still making some more changes with plots

--- Lab11-prep.jl
+++ Lab11-prep.jl
@@ -1,33 +1,23 @@
 using DataFrames
-using JSON3
+using CSV
 showln(x) = (show(x); println())
 # Read a JSON file into a DataFrame
-# df = DataFrame(JSON3.read("flat-ui-data.json"))
+df = DataFrame(CSV.read("sp_500_stock_price.csv", DataFrame))
 
 # Perform some operations on the DataFrame
 # showln(first(df, 15))  # Print the first 5 rows
+X = df[!, 1]  # Get the first column
 
-# Write the DataFrame to a JSON file
-#JSON3.write("output.json", df)
-# using CSV
-
-# Read a CSV file into a DataFrame
-# df_csv = CSV.read("flat-ui-data.csv", DataFrame)
-
-# Perform some operations on the DataFrame
-# println(last(df_csv, 15))  # Print the last 15 rows
-
+# y = df[!, 7:9]  # Get the second column
 # using GR
-using Plots
-display(histogram(randn(1000), label="histogram"))
-display(plot(rand(5), rand(5), label="scatter", seriestype=:scatter, reuse=false))
-
-x = range(0, stop=2π, length=100)
-plot(x, sin.(x), label="sin(x)", legend=:top, reuse=false)
-display(plot!(x, cos.(x), label="cos(x)", legend=:top, yflip=true, reuse=true))
-sleep(15)
+header_names = names(df)
+showln(header_names[5])
 # GICS Sector refers to the Global Industry Classification Standard sector
 # CIK refers to the Central Index Key, a unique identifier for a company
-
+using Plots
+for i in 2:10
+    display(plot!(X, df[!, i], seriestype = :line, title = "Real Earning vs. Time", xlabel = "Time", ylabel = "Real Earning", legend = false))
+end
+readline()
 # Write the DataFrame to a CSV file
 #CSV.write("output.csv", df_csv)
