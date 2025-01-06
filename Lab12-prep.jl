46024570e766023ae7245f144f9df99c79bac48d
adibML007 <a_u_z_ipe@yahoo.com>
2025-01-06 03:14:01 -0500

    adding some touch ups

---
+++ Lab12-prep.jl
@@ -0,0 +1,33 @@
+# Visualization with Julia
+using DataFrames
+using Base: LinRange
+using CSV
+showln(x) = (show(x); println())
+# Read a JSON file into a DataFrame
+df = DataFrame(CSV.read("sp_500_stock_price.csv", DataFrame))
+# println(df)
+# Perform some operations on the DataFrame
+# showln(first(df, 15))  # Print the first 5 rows
+X = df[1:100, 1]  # Get the first column
+# xt = collect(range(1, 100, 10))
+indices = 0:100
+xt = reduce(vcat, i for i in indices)
+println(xt)
+# y = df[!, 7:9]  # Get the second column
+# using GR
+header_names = names(df)
+println(header_names)
+# ["Date", "SP500", "Dividend", "Earnings", "Consumer Price Index", "Long Interest Rate", "Real Price", "Real Dividend", "Real Earnings", "PE10"]
+# showln(X)
+# GICS Sector refers to the Global Industry Classification Standard sector
+# CIK refers to the Central Index Key, a unique identifier for a company
+using Plots
+# for i in 2:10
+display(plot!(X, df[1:100, 7], seriestype = :line, title = "Real Earning vs. Time", xlabel = "Time", ylabel = "Real Earning", legend = false))
+# end
+readline()
+
+using plotlyJS
+
+# Write the DataFrame to a CSV file
+#CSV.write("output.csv", df_csv)
